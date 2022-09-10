43001dddeb6f85e8600e0c671726c18e136b44fd
Abdurahman Jafer <95960560+its0beno@users.noreply.github.com>
2022-09-10 23:06:40 +0300

    finsihed

--- gradingStudent.py
+++ gradingStudent.py
@@ -1,18 +1,51 @@
-i = int(input())
-list = []
-for a in range(i):
-    x = int(input())
-    list.append(x)
+#!/bin/python3
 
-for x in list:
-    if x + 3 > 40:
-        if x % 5 == 0:
-            print(x)
-        elif x % 5 == 3:
-            print(x+2)
-        elif x % 5 == 4:
-            print(x+1)
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+
+def gradingStudents(grades):
+
+    final = []
+    for x in grades:
+        if x + 3 >= 40:
+            if x % 5 == 0:
+                final.append(x)
+            elif x % 5 == 3:
+                final.append(x+2)
+            elif x % 5 == 4:
+                final.append(x+1)
+            else:
+                final.append(x)
         else:
-            print(x)
-    else:
-        print(x)
+            final.append(x)
+    return final
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
